7101b3de55f2fe622fae43616ecec88bc4fae6c2
janenoxid <desireejanedixon@gmail.com>
2021-03-31 17:52:15 -0500

    Finishing section 19.4.4. Still need to test to make sure it works. If not, will do another commit.

--- src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
@@ -2,9 +2,17 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.UserRepository;
 import org.launchcode.codingevents.models.User;
+import org.launchcode.codingevents.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.Optional;
 
 public class AuthenticationController {
@@ -30,10 +38,47 @@ public class AuthenticationController {
 
     }
 
-    private static void setUserSessionKey(HttpSession session, User user){
+    private static void setUserInSession(HttpSession session, User user){
         session.setAttribute(userSessionKey, user.getId());
     }
 
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegisterFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+
+    public String processRegistrationForm(@ModelAttribute @Valid RegisterFormDTO registerFormDTO,
+                                          Errors errors, HttpServletRequest request,
+                                          Model model) {
+        if (errors.hasErrors()){
+           model.addAttribute("title", "Register");
+           return "register";
+        }
+
+        User existingUser = userRepository.findByUsername(registerFormDTO.getUsername());
+
+        if (existingUser != null) {
+            errors.rejectValue("username", "username.alreadyexists", "A user with that username already exists");
+            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        String password = registerFormDTO.getPassword();
+        String verifyPassword = registerFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
+            model.addAttribute("title", "Register");
+            return "register";
+        }
+
+        User newUser = new User(registerFormDTO.getUsername(), registerFormDTO.getPassword());
+        userRepository.save(newUser);
+        setUserInSession(request.getSession(), newUser);
+
+        return "redirect:";
+    }
 
 
 }

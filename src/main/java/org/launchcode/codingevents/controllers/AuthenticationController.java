1d36bfeab6cf5c77476ae9e2120d508f3b743ed6
janenoxid <desireejanedixon@gmail.com>
2021-03-30 20:41:06 -0500

    Finishing section 19.4.3

---
+++ src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
@@ -0,0 +1,39 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.UserRepository;
+import org.launchcode.codingevents.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null){
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if(user.isEmpty()){
+            return null;
+        }
+
+        return user.get();
+
+    }
+
+    private static void setUserSessionKey(HttpSession session, User user){
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+
+
+}
